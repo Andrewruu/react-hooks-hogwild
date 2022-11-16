cffa0e86f655489bf8f740c7f9237bdcc26570d5
Andrew Wang <Andrewruujin@gmail.com>
2022-11-16 18:18:57 -0500

    commit tree 620977b80a2173053265d8dd6c6d6d318c117abb

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
+import Filter from "./Filter";
+import List from "./List";
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [isGreased, setIsGreased] = useState(false)
+	const[sortBy, setSortBy] = useState("name")
+
+	const displayHogs = hogs.filter((hog)=>(isGreased ? hog.greased : true)).sort((hogA, hogB)=>{
+		if (sortBy === "weight"){
+			return hogA.weight - hogB.weight
+		} else{
+			return hogA.name.localeCompare(hogB.name)
+		}
+	})
+
+
+
 	return (
-		<div className="App">
-			<Nav />
-		</div>
+	<div className="ui grid container App">
+      <div className="sixteen wide column centered">
+        <Nav />
+      </div>
+      <div className="sixteen wide column centered">
+        <Filter
+          showGreased={isGreased}
+          onChangeShowGreased={setIsGreased}
+          sortBy={sortBy}
+          onChangeSortBy={setSortBy}
+        />
+      </div>
+      <div className="sixteen wide column centered">
+        <List hogs={displayHogs} />
+      </div>
+    </div>
 	);
 }
 
